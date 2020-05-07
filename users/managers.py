38b3ffe4b650e952132653dd97ede7aebd341626
nebHailemariam <NebHailemariam@gmail.com>
2020-05-07 19:40:06 +0300

    UserManager fix

--- users/managers.py
+++ users/managers.py
@@ -1,27 +1,26 @@
 # users/managers.py
+
 from django.contrib.auth.models import BaseUserManager
 from . import constants as user_constants
-from .models import *
+
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, password, **extra_fields):
 
-        if not email:
-            raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        if not username:
+            raise ValueError('The username must be set')
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
-        print(user.__dict__)
         user.save()
-        if len(User.objects.filter(email=user.email)) == 0:
+        if len(self.model.objects.filter(username=user.username)) == 0:
             raise Exception("Internal error")
-        return
+        return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('user_type', user_constants.SUPERUSER)
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
