12ecb008ea9f02fd8a12091619a341d13eab885b
nebHailemariam <NebHailemariam@gmail.com>
2020-06-11 19:11:46 +0300

    Security update

--- users/views/common/security_views.py
+++ users/views/common/security_views.py
@@ -1,16 +1,15 @@
-from django.contrib.auth import login
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
 from users.serializers.common.security_serializers import LoginSerializer
 
 
-class LoginView(APIView):
-
-    def post(self, request):
+@api_view(['POST'])
+@permission_classes([])
+def login(request):
+    if request.method == "POST":
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
-        login(request, user)
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token":token.key},status=200)
